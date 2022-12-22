64990dfbc88d8b98fb66e00c7acaece884021d80
Kevin Leong <leongkevin@outlook.com>
2022-12-21 23:44:46 -0500

    Added migration, seeders & model files

---
+++ Backend/db/models/spots.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spots extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Spots.init({
+    userId: DataTypes.INTEGER,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    lat: DataTypes.DECIMAL,
+    lng: DataTypes.DECIMAL,
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.DECIMAL
+  }, {
+    sequelize,
+    modelName: 'Spots',
+  });
+  return Spots;
+};
