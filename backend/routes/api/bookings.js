eb7f9941d1bd99e033321f71d11860d20a6f38fd
Kevin Leong <leongkevin@outlook.com>
2022-12-22 23:10:37 -0500

    Added routes

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { Booking } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+	res.send('Hello World!');
+});
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
-const spotsRouter = require('./spots.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js');
+const spotImagesRouter = require('./spotimages.js');
+const bookingsRouter = require('./bookings.js');
+const reviewsRouter = require('./reviews.js');
+const reviewImagesRouter = require('./reviewimages.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.post('/test', function (req, res) {
@@ -38,10 +42,12 @@ router.use(restoreUser);
 // });
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
-
 router.use('/spots', spotsRouter);
+router.use('/spot-images', spotImagesRouter);
+router.use('/reviews', reviewsRouter);
+router.use('/review-images', reviewImagesRouter);
+router.use('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
 	res.json({ requestBody: req.body });
