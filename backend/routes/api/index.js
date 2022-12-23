3e8cd253fb9b8aed66981dbbcf7654c4f415df31
Kevin Leong <leongkevin@outlook.com>
2022-12-22 21:09:26 -0500

    Added default values

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
+const spotsRouter = require('./spots.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
@@ -25,6 +26,7 @@ router.post('/test', function (req, res) {
 // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
+
 // router.get('/restore-user', (req, res) => {
 // 	return res.json(req.user);
 // });
@@ -38,6 +40,8 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/spots', spotsRouter);
 
 router.post('/test', (req, res) => {
 	res.json({ requestBody: req.body });
