52dc17945b095425d455d133fef5f06297fceef9
leongkevin <99384279+leongkevin@users.noreply.github.com>
2023-01-01 22:06:11 -0500

    Setting correct requires

--- backend/routes/api/reviewimages.js
+++ backend/routes/api/reviewimages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { requireAuth, restoreUser } = require('../../utils/auth');
 const { Spot, User, Review, Booking, SpotImage, ReviewImage } = require('../../db/models');
 
 const { check } = require('express-validator');
@@ -8,8 +8,6 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-	res.send('Hello World!');
-});
+
 
 module.exports = router;
--- backend/routes/api/spotimages.js
+++ backend/routes/api/spotimages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { requireAuth, restoreUser } = require('../../utils/auth');
 const { Spot, User, Review, Booking, SpotImage, ReviewImage } = require('../../db/models');
 
 const { check } = require('express-validator');
@@ -8,8 +8,19 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-	res.send('Hello World!');
+router.delete('/:spotImageId', requireAuth, async (req, res) => {
+	// const spot = await SpotImage.findByPk(req.params.spotId);
+
+	// if (!spot) {
+	// 	res.status(404).json(statusCode404);
+	// } else if (spot.ownerId === req.user.id) {
+	// 	await spot.destroy();
+	// 	res.status(200).json({
+	// 		message: 'Successfully deleted',
+	// 		statusCode: 200,
+	// 	});
+	// 	// res.json({ message: spot});
+	// }
 });
 
 module.exports = router;
