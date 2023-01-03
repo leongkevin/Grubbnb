1dc340dde5809e5b14e85e6bb700a089899a2ed3
leongkevin <99384279+leongkevin@users.noreply.github.com>
2023-01-03 03:40:02 -0500

    Implemented get all bookings of current user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -15,13 +15,56 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-
-
 const statusCode404 = {
 	message: "Booking couldn't be found",
 	statusCode: 404,
 };
 
+// Get all of the Current User's Bookings
+// Return all the bookings that the current user has made.
+
+// Require Authentication: true
+
+// Request
+
+// Method: GET
+// URL: /api/user/bookings
+// Body: none
+// Successful Response
+
+// Status Code: 200
+
+router.get('/current', requireAuth, async (req, res) => {
+	let findBooking = await Booking.findAll({
+		where: { userId: req.user.id },
+		include: [
+			{
+				model: Spot,
+				attributes: { exclude: ['updatedAt', 'createdAt', 'description'] },
+				include: {
+					model: SpotImage,
+				},
+			},
+		],
+	});
+
+	const bookingsCopy = findBooking.map((el) => el.toJSON());
+
+	bookingsCopy.forEach((el) => {
+		el.Spot.previewImage = [];
+
+		el.Spot.SpotImages.forEach((SpotImages) => {
+			if (SpotImages.preview) {
+				el.Spot.previewImage = SpotImages.url;
+			}
+		});
+		el.Spot.SpotImages = undefined;
+	});
+
+	res.status(200).json({
+		Bookings: bookingsCopy,
+	});
+});
 
 // // Delete a Booking
 // // Delete an existing booking.
