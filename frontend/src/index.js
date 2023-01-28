da9823e0f8eb6fba893be6e7b566d78254537d08
leongkevin <99384279+leongkevin@users.noreply.github.com>
2023-01-28 16:13:38 -0500

    Tested demo user

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,26 +5,32 @@ import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
-  window.store = store;
+if (process.env.NODE_ENV !== 'production') {
+	restoreCSRF();
+
+	window.csrfFetch = csrfFetch;
+	window.store = store;
+	window.sessionActions = sessionActions;
 }
 
 function Root() {
-  return (
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
-  );
+	return (
+		<ReduxProvider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ReduxProvider>
+	);
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Root />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
