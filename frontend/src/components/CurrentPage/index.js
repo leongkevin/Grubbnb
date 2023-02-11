117a5118f06592bc322233552ab301c33e201fa7
leongkevin <99384279+leongkevin@users.noreply.github.com>
2023-02-11 17:26:44 -0500

    Save current page progress

--- frontend/src/components/CurrentPage/index.js
+++ frontend/src/components/CurrentPage/index.js
@@ -1,20 +1,66 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
 import CreateSpotModal from '../CreateSpotModal';
 import OpenModalButton from '../OpenModalButton';
+import * as spotActions from '../../store/spot';
+
 
 function Hosting() {
 	const dispatch = useDispatch();
 
 	const [errors, setErrors] = useState([]);
 
+	const spotComponent = useSelector((state) => Object.values(state.spot));
+
+	useEffect(() => {
+		dispatch(spotActions.getSpots());
+	}, []);
+
 	return (
 		<>
-			<h1 className="welcome-header">Welcome to Grubbnb</h1>
+									<h1>Manage Your Spots</h1>
 			<OpenModalButton
 				buttonText="Create a new listing"
 				modalComponent={<CreateSpotModal />}
 			/>
+
+<div className="grid-container">
+			{spotComponent.map((spot) => {
+				return (
+
+					<div key={spot.id} className="spot-component">
+									<h1>Manage Your Spots</h1>
+						<NavLink
+							className="spot-route"
+							to={`/spots/${spot.id}`}
+						>
+							<div className="spot-image">
+								<img
+									src={spot.previewImage}
+									className="spot-thumbnail"
+								></img>
+							</div>
+							<div className="spot-details">
+								<div className="spot-details-title">
+									<div className="spot-city-country">
+										{spot.city}, {spot.country}
+									</div>
+									<div className="spot-rating">
+										☆{spot.avgRating}
+									</div>
+								</div>
+								<div className="spot-description">
+									{spot.description}
+								</div>
+								<div className="spot-price">${spot.price} night</div>
+							</div>
+						</NavLink>
+					</div>
+				);
+			})}
+		</div>
 		</>
 	);
 }
