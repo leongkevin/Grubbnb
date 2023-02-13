174e64f34fbecd82b9c5e4b6cdff24ac5a0d34c8
leongkevin <99384279+leongkevin@users.noreply.github.com>
2023-02-12 21:37:31 -0500

    Added authentication requirement for /current and spot delete and edit

--- frontend/src/components/CurrentPage/index.js
+++ frontend/src/components/CurrentPage/index.js
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom';
 import { useEffect } from 'react';
 import CreateSpotModal from '../CreateSpotModal';
 import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 import * as spotActions from '../../store/spot';
+import * as sessionActions from '../../store/session';
 import './CurrentPage.css';
 
 function Hosting() {
@@ -13,59 +16,85 @@ function Hosting() {
 	const [errors, setErrors] = useState([]);
 
 	const spotComponent = useSelector((state) => Object.values(state.spot));
+	const sessionUser = useSelector((state) => state.session.user);
 
 	useEffect(() => {
 		dispatch(spotActions.getSpots());
 	}, []);
-
-	return (
-		<>
-			<div className="current-page-title">Manage Your Spots</div>
-			<div className="current-page-menu">
-				<div className="current-page-create-spot spot-thumbnail ">
-					<OpenModalButton
-						buttonText="+ Create a new spot"
-						modalComponent={<CreateSpotModal />}
-					/>
+	if (sessionUser) {
+		return (
+			<>
+				<div className="current-page-title">Manage Your Spots</div>
+				<div className="current-page-menu">
+					<div className="current-page-create-spot spot-thumbnail ">
+						<OpenModalButton
+							buttonText="+ Create a new spot"
+							modalComponent={<CreateSpotModal />}
+						/>
+					</div>
 				</div>
-			</div>
-			<div className="grid-container current-page-grid">
-				{spotComponent.map((spot) => {
-					return (
-						<div key={spot.id} className="spot-component">
-							<NavLink
-								className="spot-route"
-								to={`/spots/${spot.id}`}
-							>
-								<div className="spot-image">
-									<img
-										src={spot.previewImage}
-										className="spot-thumbnail"
-									></img>
-								</div>
-								<div className="spot-details">
-									<div className="spot-details-title">
-										<div className="spot-city-country">
-											{spot.city}, {spot.country}
+				<div className="grid-container current-page-grid">
+					{spotComponent.map((spot) => {
+						console.log(spot.ownerId);
+						if (spot.ownerId === sessionUser.id) {
+							return (
+								<div key={spot.id} className="spot-component">
+									<NavLink
+										className="spot-route"
+										to={`/spots/${spot.id}`}
+									>
+										<div className="spot-image">
+											<img
+												src={spot.previewImage}
+												className="spot-thumbnail"
+											></img>
 										</div>
-										<div className="spot-rating">
-											☆{spot.avgRating}
+										<div className="spot-details">
+											<div className="spot-details-title">
+												<div className="spot-city-country">
+													{spot.city}, {spot.country}
+												</div>
+												<div className="spot-rating">
+													☆{spot.avgRating}
+												</div>
+											</div>
+											<div className="spot-description">
+												{spot.description}
+											</div>
+											<div className="spot-price">
+												${spot.price} night
+											</div>
 										</div>
-									</div>
-									<div className="spot-description">
-										{spot.description}
-									</div>
-									<div className="spot-price">
-										${spot.price} night
-									</div>
+									</NavLink>
 								</div>
-							</NavLink>
-						</div>
-					);
-				})}
-			</div>
-		</>
-	);
+							);
+						}
+					})}
+				</div>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<div className="current-page-title">Authentication Required</div>
+				<div className="current-page-menu">
+					<div className="current-page-create-spot spot-thumbnail ">
+						<OpenModalButton
+							buttonText=" Login to Grubbnb"
+							modalComponent={<LoginFormModal />}
+						/>
+					</div>
+					<div className="or-div">or</div>
+					<div className="current-page-create-spot spot-thumbnail ">
+						<OpenModalButton
+							buttonText="Sign up for Grubbnb"
+							modalComponent={<SignupFormModal />}
+						/>
+					</div>
+				</div>
+			</>
+		);
+	}
 }
 
 export default Hosting;
--- frontend/src/components/SpotPage/index.js
+++ frontend/src/components/SpotPage/index.js
@@ -7,17 +7,19 @@ import OpenModalButton from '../OpenModalButton';
 import DeleteSpotButton from '../DeleteSpotButton';
 import EditSpotModal from '../EditSpotModal';
 import ReviewsPage from '../ReviewsGetAll';
+import * as sessionActions from '../../store/session';
 
 function SpotPage() {
 	const dispatch = useDispatch();
 	const { spotId } = useParams();
 
 	const spotComponent = useSelector((state) => Object.values(state.spot));
+	const sessionUser = useSelector((state) => state.session.user);
 
 	useEffect(() => {
 		dispatch(spotActions.getSpots());
 	}, []);
-
+	if (sessionUser) {
 	return (
 		<div className="spot-page-container">
 			{spotComponent.map((spot) => {
@@ -108,6 +110,89 @@ function SpotPage() {
 			})}
 		</div>
 	);
+
+} else {
+    return (
+		<div className="spot-page-container">
+			{spotComponent.map((spot) => {
+				// console.log(`this is line 26: ${spotId} === ${spot.id}`)
+                let rating = spot.avgRating;
+                if(rating === null) {
+                    console.log(rating)
+                    rating = "none"
+                }
+				if (parseInt(spotId) === spot.id) {
+					return (
+						<div key={spot.id} className="spot-page-result">
+							<div className="spot-header">
+								<div className="spot-title">
+									{spot.name}
+									<div className="spot-subtitle">
+										<div className="spot-subtitle-rating">
+                                         ☆ {rating}
+										</div>
+                                        <div>·</div>
+										<div className="spot-address">
+											{spot.city}, {spot.state},{' '}
+											{spot.country}
+										</div>
+									</div>
+								</div>
+								<div className="spot-image">
+									<div className="spot-image-divider">
+										<div className="spot-image-divider-left">
+											<img
+												src={spot.previewImage}
+												className="spot-image-main"
+											></img>
+										</div>
+										{/* <div className="spot-image-divider-right">
+											<img
+												src={spot.previewImage}
+												className="spot-image-secondary"
+											></img>
+											<img
+												src={spot.previewImage}
+												className="spot-image-third"
+											></img>
+											<img
+												src={spot.previewImage}
+												className="spot-image-fourth"
+											></img>
+											<img
+												src={spot.previewImage}
+												className="spot-image-fifth"
+											></img>
+										</div> */}
+									</div>
+								</div>
+								<div className="spot-page-details">
+									<div className="spot-page-details-left">
+										{/* {' '}
+										Entire home hosted by ownerId:{' '}
+										{spot.ownerId} */}
+										<div className="spot-description">
+											{spot.description}
+										</div>
+                                        <ReviewsPage />
+									</div>
+									<div className="spot-page-details-right">
+
+
+
+									</div>
+								</div>
+							</div>
+						</div>
+					);
+				}
+			})}
+		</div>
+
+
+
+    )
+}
 }
 
 export default SpotPage;
